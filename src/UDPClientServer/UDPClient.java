fc3c95c16933a379d3a2927c22c8a2976c828556
zaynashahzad <zaynashahzad24@gmail.com>
2014-05-10 11:48:42 -0700

    started from the bottom, now im here

--- src/UDPClientServer/UDPClient.java
+++ src/UDPClientServer/UDPClient.java
@@ -21,27 +21,35 @@ class UDPClient
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setVisible(true);*/
 
-      BufferedReader inFromUser =
-         new BufferedReader(new InputStreamReader(System.in));
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
       DatagramSocket clientSocket = new DatagramSocket();
       InetAddress IPAddress = InetAddress.getByName("192.168.43.160");
       byte[] sendData = new byte[1024];
       byte[] receiveData = new byte[1024];
+
    while(true){
+      try{
       String sentence = inFromUser.readLine();
       sendData = sentence.getBytes();
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
       clientSocket.send(sendPacket);
+      clientSocket.setSoTimeout(1000);
       DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
       clientSocket.receive(receivePacket);
       String modifiedSentence = new String(receivePacket.getData());
+      System.out.println("FROM SERVER:" + modifiedSentence);
+
+    }catch (SocketTimeoutException e) {
+                // timeout exception.
+                System.out.println("Timeout reached!!! " + e);
+            }
+
     /*  if (modifiedSentence.contains("true")){
          System.out.println("YOU SENT ME TRUE");
       }else{
          System.out.println("YOU SENT ME FALSE. SADFACE!!");
       }*/
    
-          System.out.println("FROM SERVER:" + modifiedSentence);
      // clientSocket.close();
       }
 
