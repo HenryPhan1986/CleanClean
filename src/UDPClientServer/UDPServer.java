ecface5e241354e55f42b0e469b47ebb5716ff7f
jamonroe <jasonmonroe90@gmail.com>
2014-05-10 14:23:07 -0700

    Pushed Server

--- src/UDPClientServer/UDPServer.java
+++ src/UDPClientServer/UDPServer.java
@@ -4,11 +4,9 @@ import java.io.*;
 import java.net.*;
 import java.util.concurrent.locks.ReentrantLock;
 
-class UDPServer
-{
+class UDPServer {
 	private static final ReentrantLock lock = new ReentrantLock();
-	private static Boolean collision = false;
-	public static final long SLOT_TIME_NANOS = 800000L;
+	public static final int SLOT_TIME_NANOS = 800000;
 	private static DatagramSocket serverSocket;
 	static {
 		try {
@@ -17,63 +15,73 @@ class UDPServer
 			e.printStackTrace();
 		}
 	}
-	
-   public static void main(String args[]) throws Exception
-      {
-         byte[] receiveData;
-         while(true)
-           {
-        	  receiveData = new byte[1024];
-              DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-              serverSocket.receive(receivePacket);
-              final InetAddress IPAddress = receivePacket.getAddress();
-              final int port = receivePacket.getPort();
-              final String sentence = new String( receivePacket.getData());
-              System.out.println("RECEIVED: " + sentence.substring(0, sentence.indexOf('\0')));
-              
-              if (!lock.isLocked()) {
-            	  new Thread(new Runnable() {
-					public void run() {
-						try {
-							if (lock.tryLock()) {
-								long startTime = System.nanoTime();
-								while (System.nanoTime() - startTime < SLOT_TIME_NANOS) 
-								{
-									if (collision) {
-										sendCollision(IPAddress, port, sentence);
-										collision = false;
-										lock.unlock();
-										return;
-									}
-								}
-								lock.unlock();
-								sendSuccess(IPAddress, port, sentence);
-							} else {
-								System.out.println("SHIT GOT MESSED UP");
-							}
-						} catch (Exception e) {
-							e.printStackTrace();
+
+	public static void main(String args[]) throws Exception {
+		byte[] receiveData;
+		while (true) {
+			receiveData = new byte[1024];
+			DatagramPacket receivePacket = new DatagramPacket(receiveData,
+					receiveData.length);
+			serverSocket.receive(receivePacket);
+			final InetAddress IPAddress = receivePacket.getAddress();
+			final int port = receivePacket.getPort();
+			final String sentence = new String(receivePacket.getData());
+
+			System.out.println("RECEIVED: "
+					+ sentence.substring(0, sentence.indexOf('\0')));
+
+			new Thread(new Runnable() {
+				public void run() {
+					try {
+						if (lock.isLocked()) {
+							lock.lockInterruptibly();
+							Thread.sleep(0, SLOT_TIME_NANOS);
+							lock.unlock();
+							sendCollision(IPAddress, port, sentence);
+						} else {
+							lock.lockInterruptibly();
+							Thread.sleep(0, SLOT_TIME_NANOS);
+							lock.unlock();
+							sendSuccess(IPAddress, port, sentence);
 						}
-					} 
-            	  }).start();
-              } else {
-            	  sendCollision(IPAddress, port, sentence);
-            	  collision = true;
-              }
-           }
-      }
-   
-   public static void sendCollision(InetAddress IPAddress, int port, String message) throws IOException {
-	   System.out.println("SENT: FALSE");
-	   String sentence = "false " + message;
-       DatagramPacket sendPacket = new DatagramPacket(sentence.getBytes(), sentence.length(), IPAddress, port);
-       serverSocket.send(sendPacket);
-   }
-   
-   public static void sendSuccess(InetAddress IPAddress, int port, String message) throws IOException {
-	   System.out.println("SENT: TRUE");
-	   String sentence = "true " + message;
-       DatagramPacket sendPacket = new DatagramPacket(sentence.getBytes(), sentence.length(), IPAddress, port);
-       serverSocket.send(sendPacket);
-   }
+					}
+					catch (InterruptedException ie) {
+						while (lock.isLocked()) { }
+						sendCollision(IPAddress, port, sentence);
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+						if (lock.isHeldByCurrentThread())
+							lock.unlock();
+					}
+				}
+			}).start();
+		}
+	}
+
+	public static void sendCollision(InetAddress IPAddress, int port,
+			String message) {
+		String sentence = "FALSE";
+		System.out.println(sentence + ": " + message.substring(0, message.indexOf('\0')));
+		DatagramPacket sendPacket = new DatagramPacket(sentence.getBytes(),
+				sentence.length(), IPAddress, port);
+		try {
+			serverSocket.send(sendPacket);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	public static void sendSuccess(InetAddress IPAddress, int port,
+			String message) throws IOException {
+		String sentence = "TRUE";
+		System.out.println(sentence + ": " + message.substring(0, message.indexOf('\0')));
+		DatagramPacket sendPacket = new DatagramPacket(sentence.getBytes(),
+				sentence.length(), IPAddress, port);
+		try {
+			serverSocket.send(sendPacket);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
 }
